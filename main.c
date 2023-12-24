608dbcb1c20ffc77418c18e1b7dbfe5c9a0fde2d
KLU5220505003 <128073582+Beg1mjohn@users.noreply.github.com>
2023-12-24 20:20:50 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+
+      typedef struct {
+      int teklifveren_sayisi;
+      double teklif;
+    } Teklif;
+      
+      int teklif_sayisi;
+      printf("Teklif veren sayisini giriniz: ");
+      scanf("%d", &teklif_sayisi);
+
+     
+      Teklif teklifler[teklif_sayisi];
+
+      int i;
+      for (i = 0; i < teklif_sayisi; i++) {
+        printf("Teklif veren %d, teklifinizi giriniz: ", i + 1);
+        scanf("%lf", &teklifler[i].teklif);
+      }
+
+    
+      double en_yuksek_teklif = teklifler[0].teklif;
+      int kazanan_teklifveren_no = teklifler[0].teklifveren_sayisi;
+      
+      for ( i = 1; i < teklif_sayisi; i++) {
+        if (teklifler[i].teklif > en_yuksek_teklif) {
+          en_yuksek_teklif = teklifler[i].teklif;
+          kazanan_teklifveren_no = teklifler[i].teklifveren_sayisi;
+        }
+      }
+
+    
+      printf("Kazanan teklif veren: %d\n", kazanan_teklifveren_no);
+      printf("Odeme: %.2f TL\n", en_yuksek_teklif);
+
+      return 0;
+    }
